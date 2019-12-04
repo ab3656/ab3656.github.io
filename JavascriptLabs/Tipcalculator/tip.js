bb74a9f0b22b3f0978860584c83a8ece239f0bf0
ab3656 <ameetbht@gmail.com>
2019-12-04 11:12:44 -0600

    Update tip.js

--- JavascriptLabs/Tipcalculator/tip.js
+++ JavascriptLabs/Tipcalculator/tip.js
@@ -1,9 +1,10 @@
 function calcTip() {
-	var subtotalElem = document.getElementById("subtotal");
-	var tipElem = document.getElementById("tip");
+	var subtotalElem = document.getElementById("subtotal").value;
+	var tipElem = document.getElementById("tip").value;
 	var totalElem = document.getElementById('total');
 	var subtotal = parseFloat(subtotalElem);
 	var tip = parseFloat(tipElem);
-	var total = subtotal*tip/100;
-	totalElem.innerHTML = '$' + total;
+    var total =  subtotal + subtotal*tip/100;
+    totalElem.innerHTML = '$' + total;
+    
 }
