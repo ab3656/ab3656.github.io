bf313a81fe1672e75adfd54241cda4acb48bdb3d
ab3656 <ameetbht@gmail.com>
2019-12-05 18:24:03 -0600

    Update decoratemytext.js

--- DecorateMyText/decoratemytext.js
+++ DecorateMyText/decoratemytext.js
@@ -1,24 +1,26 @@
-function showDecorations(){
-    setInterval(startDec, 500);    
+function showDecorations() {
+    setInterval(startDec, 500);
 }
 
-function startDec(){
+function startDec() {
     var mytext = document.getElementById("myText");
-    var size = window.getComputedStyle(mytext).fontSize;
-    size = parseInt(size);
-    size +=2;
-    mytext.style.fontSize = size + "pt";
+    if (mytext.value != "") {
+        var size = window.getComputedStyle(mytext).fontSize;
+        size = parseInt(size);
+        size += 2;
+        mytext.style.fontSize = size + "pt";
+    }
 }
 
-function showBling(){
-    
+function showBling() {
+
     var myText = document.getElementById("myText");
-    if(document.getElementById("myCheckbox").checked) {
+    if (document.getElementById("myCheckbox").checked) {
         myText.style.fontWeight = "bold";
         myText.style.color = "green";
         myText.style.textDecoration = "underline";
-        document.body.style.backgroundImage = 
-        "url('www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+        document.body.style.backgroundImage =
+            "url('www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
     }
     else {
         myText.style.fontWeight = "normal";
@@ -30,15 +32,15 @@ function showBling(){
 
 function malkovitch() {
     let regex = /(\b\w{5,}\b)/;
-    
+
     var mytext = document.getElementById("myText");
     var res = mytext.value.split(" ");
-    for(var i = 0; i < res.length; i++) {
-        if(regex.test(res[i])) {            
+    for (var i = 0; i < res.length; i++) {
+        if (regex.test(res[i])) {
             res[i] = "Malkovich";
         }
     }
-    
+
     mytext.value = res.join(" ");
 
 }
