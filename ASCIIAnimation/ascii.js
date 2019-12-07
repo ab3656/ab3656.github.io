8693515e4dd68b3e90430d286c9b694d7bd18ac5
ab3656 <ameetbht@gmail.com>
2019-12-07 11:16:28 -0600

    added styles

---
+++ ASCIIAnimation/ascii.js
@@ -0,0 +1,27 @@
+window.onload = setDefaultValues;
+var animation = "";
+
+function setDefaultValues(){
+    document.getElementById("stop").disabled = true;
+    
+}
+
+function startAnimation() {
+
+}
+
+function stopAnimation(){
+
+}
+
+function setSize() {
+
+}
+
+function setSpeed() {
+
+}
+
+function setAnimation() {
+    animation = ANIMATIONS[document.getElementById("animation").value];
+}
