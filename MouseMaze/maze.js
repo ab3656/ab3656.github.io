bdb8d3f3244132ff9dcb1648eaabbde2de41b98a
ab3656 <ameetbht@gmail.com>
2019-12-12 14:49:18 -0600

    still changes needed

--- MouseMaze/maze.js
+++ MouseMaze/maze.js
@@ -1,31 +1,32 @@
 var wallHit = false;
 var start = false;
-$(document).ready(function () {    
-    $("#start").click(start);    
-    $("#end").mouseover(end);     
+var end = false;
+$(document).ready(function () {
+    $("#start").click(start);
+    $("#end").mouseover(end);
 });
 
 var move = function () {
     $(".boundary").addClass("youlose");
+    $("#status").text("Move the mouse without touching the walls!");
     wallHit = true;
 };
 
-var end = function () {
-    if(!wallHit) {        
-        $("#status").text("You won! :]");           
-    }
-    else {
-        $("#status").text("Sorry, you lost! :[");
+var end = function () {    
+    if (start) {
+        end = true;
+        if (!wallHit) {
+            $("#status").text("You won! :]");
+        } else {
+            $("#status").text("Sorry, you lost! :[");
+        }
     }
 };
 
-var start = function(){
-    $(".boundary").mouseover(move);  
+var start = function () {
     start = true;
-    if(!wallHit) 
-    {
-        $("#status").text("Move the mouse without touching the walls!");
-    }
+    $(".boundary").mouseover(move);
+    $("#status").text("Move the mouse without touching the walls!");
     wallHit = false;
     $(".boundary").removeClass("youlose");
 };
