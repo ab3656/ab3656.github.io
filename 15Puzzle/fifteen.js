b6c84629194bfac9d8ff1fc07435390dcbf799cf
ab3656 <ameetbht@gmail.com>
2019-12-14 12:04:07 -0600

    Update fifteen.js

--- 15Puzzle/fifteen.js
+++ 15Puzzle/fifteen.js
@@ -3,26 +3,21 @@ $(function(){
     init();
 });
 
-init = function() {
-    var divs = $("#puzzlearea").find("div");    
-      
-    // initialize each piece
-    for (var i=0; i< divs.length; i++) {
-        var div = divs[i];
-        
+var init = function() {
+    $("#puzzlearea > div").each(function(i){
         // calculate x and y for this piece
         var x = ((i % 4) * 100) ;
         var y = (Math.floor(i / 4) * 100) ;
 
         // set basic style and background
-        div.className = "puzzlepiece";
-        div.style.left = x + 'px';
-        div.style.top = y + 'px';
-        div.style.backgroundImage = 'url("background.jpg")';
-        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
+        $(this).addClass("puzzlepiece");
+        $(this).css("left", x + 'px');
+        $(this).css("top", y + 'px');
+        $(this).css("background-image", 'url("background.jpg")');
+        $(this).css("background-position", -x + 'px ' + (-y) + 'px');
         
         // store x and y for later
-        div.x = x;
-        div.y = y; 
-    }        
+        $(this).x = x;
+        $(this).y = y; 
+    })       
 };
